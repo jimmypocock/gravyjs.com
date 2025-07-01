cef005be52e3221328270ed4f360b0d898e071a3
jimmypocock <jimmypocock@yahoo.com>
2025-07-01 00:02:13 -0500

    fix: setup for using slate

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // Environment variables prefixed with VITE_ are automatically exposed
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
 });
